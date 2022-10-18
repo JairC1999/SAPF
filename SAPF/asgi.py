f04fd44dece5a7c2fe71174cb22f4342c6c20e82
FelipeDamiao <felipedamiao81@gmail.com>
2022-10-18 17:05:36 -0300

    Coloquei arquivos do projeto
    Alterei os arquivos da raiz do projeto, alterei a pagina de recuperação de login e coloquei dois models.

--- SAPF/asgi.py
+++ SAPF/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for SADPE project.
+ASGI config for SAPF project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SADPE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SAPF.settings')
 
 application = get_asgi_application()
--- paginas/models.py
+++ paginas/models.py
@@ -1,3 +1,9 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
+class TestModel(models.Model):
+    test1 = models.CharField('teste',max_length=100)
+    test2 = models.CharField('teste2',max_length=50, blank=True)
