2e4fa51f3ca5e25c0ca7b095ef630fb6df15cc0c
FelipeDamiao <felipedamiao81@gmail.com>
2022-10-24 18:19:26 -0300

    Fiz umas pequenas alterações
    Alterei as rotas da páginas

--- paginas/views.py
+++ paginas/views.py
@@ -2,5 +2,5 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = "paginainicial.html" 
+    template_name = "login.html" 
     
