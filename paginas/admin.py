21583e13fe938f43c13f626bda6a223450aa0f42
FelipeDamiao <felipedamiao81@gmail.com>
2022-10-25 16:31:25 -0300

    Tirei umas páginas e adicionei migrações

--- paginas/admin.py
+++ paginas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from paginas.models import Persor
+
 # Register your models here.
+
+admin.site.register(Persor)
--- paginas/models.py
+++ paginas/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 # Create your models here.
 
+class Persor(models.Model):
+#quando não definir uma primary key
+   first_name =  models.CharField('primeiro nome',max_length=30)
+   last_name = models.CharField('segundo nome',max_length=30)
+   telefone = models.CharField('telefone',max_length=19, null=True)
 
 class TestModel(models.Model):
     test1 = models.CharField('teste',max_length=100)
